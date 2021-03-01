79be595a330a98bb0570391dd47ed1f9535d68f9
Clyde Leef <chl51@georgetown.edu>
2021-03-01 10:19:39 -0500

    working on main

--- main.cc
+++ main.cc
@@ -10,12 +10,57 @@ int main(int argc, char *argv[]) {
             cout << "need file argument" << endl;
         
         File.open(argv[1]);
-    
 
 
         BST tree;
         BST* root = NULL;
-        root = tree.insert(root, 50);
+        int hold;
+        string throwaway;
+
+        File >> hold;
+        root = tree.insert(root, hold);
+
+        while(File >> hold) {
+            cout << "hold is: " << hold << endl;
+            tree.insert(root, hold);
+        }
+        
+        cout << "hold is now: " << endl;
+        
+        tree.display(root);
+        cout << endl;
+        
+        char throwaway2;
+        File >> throwaway2;
+        cout << "throwaway2: " << throwaway2 << endl;
+        getline(File, throwaway);
+        cout << "throwaway: " << throwaway << endl;
+        getline(File, throwaway);
+        cout << "throwaway: " << throwaway << endl;
+        getline(File, throwaway);
+        cout << "throwaway: " << throwaway << endl;
+        getline(File, throwaway);
+        cout << "throwaway: " << throwaway << endl;
+
+        while(File >> hold) {
+            cout << "in search while, searching for: " << hold << endl;
+            tree.search(root, hold);
+        }
+
+        tree.display(root);
+        cout << endl;
+
+        File >> throwaway;
+        File >> throwaway;
+        
+        while(File >> hold) {
+            cout << "in remove while, removing: " << hold << endl;
+            tree.remove(root, hold);
+        }
+
+        tree.display(root);
+        cout << endl;
+/*
         tree.insert(root, 30);
         tree.insert(root, 40);
         tree.insert(root, 20);
@@ -26,20 +71,15 @@ int main(int argc, char *argv[]) {
         tree.insert(root, 46);
         tree.insert(root, 44);
         tree.insert(root, 35);
+*/
 
 
-        tree.display(root);
-        cout << endl;
 
-        tree.remove(root, 30);
+       // tree.remove(root, 30);
 
-        tree.display(root);
-        cout << endl;
 
-        tree.remove(root, 80);
+      //  tree.remove(root, 80);
 
-        tree.display(root);
-        cout << endl;
 
     }
     catch(exception e){
